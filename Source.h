419575811534989c00311286700de1250c201b83
Oleg Muromtsev <145152119+OlegMSU@users.noreply.github.com>
2024-07-16 14:00:24 +0300

    Create Source.h

---
+++ Source.h
@@ -0,0 +1,22 @@
+#include <memory>
+#include <iostream>
+
+#pragma once
+
+template <typename NumType = unsigned, typename SizeType = unsigned, class Allocator = std::allocator<unsigned>>
+class BoundedIntSet
+{
+public:
+    Allocator SetAllocator;
+    SizeType max_size;
+    SizeType size;
+    NumType* set_ptr;
+    BoundedIntSet(SizeType maxsize);
+    BoundedIntSet(SizeType maxsize, NumType single_element);
+    void Unite(BoundedIntSet<NumType, SizeType, Allocator>* second_set);
+    bool UnionCheck(BoundedIntSet<NumType, SizeType, Allocator>* second_set);
+    bool Add(NumType new_element, bool checked);
+    void Print();
+    SizeType Find(NumType num);
+    ~BoundedIntSet();
+};
