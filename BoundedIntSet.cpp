586d84f4a5a55e704cf317becd6ba3fbb9975164
Oleg Muromtsev <145152119+OlegMSU@users.noreply.github.com>
2024-07-16 11:03:50 +0300

    Update BoundedIntSet.cpp

--- BoundedIntSet.cpp
+++ BoundedIntSet.cpp
@@ -1,220 +1,138 @@
-#include <iostream>
-#include <unordered_set>
-#include <string>
-#include <stdlib.h>
-#include <algorithm>
 #include <chrono>
-#include <vector>
+#include <iostream>
+#include <memory>
+#include <unordered_set>
 
-const unsigned PRIME = 3;
-
-unsigned Hash_fun(unsigned num)
-{
-    return num % 97;
-}
-
+template <typename NumType = unsigned, typename SizeType = unsigned, class Allocator = std::allocator<unsigned>>
 class BoundedIntSet
 {
 public:
-    struct Set_Elem
+    Allocator SetAllocator;
+    SizeType max_size;
+    NumType* set_ptr;
+    SizeType size;
+    BoundedIntSet(SizeType maxsize)
     {
-        unsigned data;
-        struct Set_Elem* next;
-    };
-    unsigned MAXSIZE;
-    struct Set_Elem* set_ptr;
-    unsigned set_size;
-    struct Set_Elem* buf_ptr;
-    const unsigned mask = 0x80000000;
-    BoundedIntSet(unsigned maxsize)
-    {
-        this->MAXSIZE = maxsize;
-        set_ptr = (struct Set_Elem*)malloc((MAXSIZE + PRIME - 1) * sizeof(Set_Elem));
-        buf_ptr = set_ptr + PRIME;
-        set_size = 0;
-        for (auto i = 0; i < (MAXSIZE - 1 + PRIME); i++)
-        {
-            set_ptr[i].data = 0;
-            set_ptr[i].next = NULL;
-        }
+        max_size = maxsize;
+        size = 0;
+        set_ptr = SetAllocator.allocate(maxsize);
     }
-    BoundedIntSet(unsigned maxsize, unsigned single_element)
+    BoundedIntSet(SizeType maxsize, NumType single_element)
     {
-        this->MAXSIZE = maxsize;
-        set_ptr = (struct Set_Elem*)malloc((MAXSIZE + PRIME - 1) * sizeof(Set_Elem));
-        buf_ptr = set_ptr + PRIME;
-        set_size = 0;
-        for (auto i = 0; i < (MAXSIZE - 1 + PRIME); i++)
-        {
-            set_ptr[i].data = 0;
-            set_ptr[i].next = NULL;
-        }
-        this->Add(single_element);
+        max_size = maxsize;
+        size = 0;
+        set_ptr = SetAllocator.allocate(maxsize);
+        this->Add(single_element, 1);
     }
-    void Unite(BoundedIntSet* set)
+    void Unite(BoundedIntSet<NumType, SizeType, Allocator>* second_set)
     {
-        struct Set_Elem* head;
-        for (auto i = 0; i < PRIME; i++)
+        SizeType index = 0;
+        SizeType n = this->size;
+        SizeType m = second_set->size;
+        SizeType i = 0, j = 0;
+        while ((i < n) && (j < m))
         {
-            head = (set->set_ptr) + i;
-            if (head->data >> 31)
+            if (this->set_ptr[i] == second_set->set_ptr[j])
             {
-                if (!this->Find(head->data ^ mask))
-                {
-                    this->Add(head->data ^ mask);
-                }
-                while (head->next != NULL)
-                {
-                    head = head->next;
-                    if (!this->Find(head->data ^ mask)) this->Add(head->data ^ mask);
-                }
-
+                i++;
+                j++;
             }
+            else {
+                if (this->set_ptr[i] < second_set->set_ptr[j]) {
+                    i++;
+                }
+                else {
+                    this->Add(second_set->set_ptr[j], 1);
+                    j++;
+                }
+            }
+        }
+        while (j < m) {
+            this->Add(second_set->set_ptr[j], 1);
+            j++;
         }
     }
     bool UnionCheck(BoundedIntSet* second_set)
     {
-        unsigned cnt = this->set_size;
-        struct Set_Elem* head;
-        if (this->MAXSIZE != second_set->MAXSIZE) return false;
-        for (auto i = 0; i < PRIME; i++)
+        
+        SizeType cnt = 0;
+        SizeType n = this->size;
+        SizeType m = second_set->size;
+        SizeType i = 0,  j = 0;
+        while ((i < n) && (j < m))
         {
-            head = (second_set->set_ptr) + i;
-            if (head->data >> 31)
+            if (this->set_ptr[i] == second_set->set_ptr[j])
             {
-                if (!this->Find(head->data ^ mask)) cnt++;
-                while (head->next != NULL)
-                {
-                    head = head->next;
-                    if (!this->Find((head->data ^ mask))) cnt++;
+                cnt++;
+                i++;
+                j++;
+            }
+            else {
+                if (this->set_ptr[i] < second_set->set_ptr[j]) {
+                    cnt++;
+                    i++;
                 }
-
+                else {
+                    cnt++;
+                    j++; 
+                }
             }
         }
-        if ((cnt <= MAXSIZE) && (cnt != this->set_size)) return true;
+        while (i < n) {
+            cnt++;
+            i++;
+        }
+        while (j < m) {
+            cnt++;
+            j++;
+        }
+        if ((cnt <= this->max_size)&&(cnt > this->size)) return true;
         else return false;
     }
-    unsigned Add(unsigned new_element)
+    bool Add(NumType new_element, bool checked)
     {
-        if (this->set_size == MAXSIZE) return 0;
-        unsigned index = Hash_fun(new_element) % PRIME;
-        struct Set_Elem* head = (this->set_ptr) + index;
-        if ((head->data >> 31) == 0)
+        if ((this->size + 1 <= this->max_size)&&(checked || (this->Find(new_element) == this->max_size)))
         {
-            head->data = new_element + mask;
-            this->set_size++;
-            return 1;
-        }
-        else if ((head->data ^ mask) == new_element)
-        {
-            return 0;
-        }
-        else
-        {
-            while (head->next != NULL)
+            SizeType i = 0;
+            while ((this->set_ptr[i] < new_element) && (i < this->size)) i++;
+            SizeType j = this->size;
+            while ((j > i))
             {
-                if ((head->data ^ mask) == new_element) break;
-                head = head->next;
+                this->set_ptr[j] = this->set_ptr[j - 1];
+                j--;
             }
-            if ((head->data ^ mask) != new_element)
-            {
-                head->next = buf_ptr;
-                head = head->next;
-                head->data = new_element + mask;
-                buf_ptr += 1;
-                this->set_size++;
-                return 1;
-            }
-            return 0;
+            this->set_ptr[i] = new_element;
+            this->size++;
         }
+        else return false;
     }
 
     void Print()
     {
-        for (auto i = 0; i < PRIME; i++)
+        for (auto i = 0; i < size; i++)
         {
-            struct Set_Elem* head = (this->set_ptr) + i;
-            if (head->data >> 31)
-            {
-                std::cout << (head->data ^ mask) << ' ';
-                while (head->next != NULL)
-                {
-                    head = head->next;
-                    std::cout << (head->data ^ mask) << ' ';
-                }
-
-            }
+            std::cout << set_ptr[i] << ' ';
         }
         std::cout << '\n';
     }
-    bool Find(unsigned num)
+    SizeType Find(NumType num)
     {
-        struct Set_Elem* head = this->set_ptr + Hash_fun(num) % PRIME;
-        while (((head->data ^ mask) != num) && (head->next != NULL)) head = head->next;
-        if ((head->data ^ mask) == num) return true;
-        else return false;
-
+        if (this->size == 0) return this->max_size;
+        bool result = false;
+        SizeType l = 0;
+        SizeType r = this->size - 1;
+        SizeType mid = (l + r) / 2;
+        while ((l <= r) && (result != true) && (mid != 0)) {
+            mid = (l + r) / 2; 
+            if (this->set_ptr[mid] == num) result = true; 
+            if (this->set_ptr[mid] > num) r = mid - 1; 
+            else l = mid + 1;
+        }
+        if (result) return mid;
+        else return this->max_size;
     }
     ~BoundedIntSet()
     {
-        if (set_ptr != NULL) free(set_ptr);
+        SetAllocator.deallocate(set_ptr, max_size);
     }
 };
-
-
-int main()
-{
-    srand(time(0));
-
-    const unsigned amount = 10000;
-    const unsigned cycles = 10000;
-    const unsigned k = 20;
-
-
-
-
-    BoundedIntSet* arr[amount];
-    std::unordered_set<unsigned>* arr2[amount];
-    for (auto i = 0; i < amount; i++)
-    {
-        unsigned r = rand();
-        arr[i] = new BoundedIntSet(k, r);
-        arr2[i] = new std::unordered_set<unsigned>{ r };
-    }
-
-
-    auto sum1 = 0;
-    auto sum2 = 0;
-    auto len = 0;
-    for (auto i = 0; i < cycles; i++)
-    {
-        unsigned i1 = rand() % amount;
-        unsigned i2 = rand() % amount;
-        auto start_time = std::chrono::steady_clock::now();
-        if (arr[i1]->UnionCheck(arr[i2])) {
-            auto end_time = std::chrono::steady_clock::now();
-            auto elapsed_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
-            std::cout << elapsed_ns.count() << " ns ";
-            start_time = std::chrono::steady_clock::now();
-            arr[i1]->Unite(arr[i2]);
-            end_time = std::chrono::steady_clock::now();
-            elapsed_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
-            sum1 += elapsed_ns.count();
-            std::cout << elapsed_ns.count() << " ns ";
-            start_time = std::chrono::steady_clock::now();
-            arr2[i1]->merge(*arr2[i2]);
-            end_time = std::chrono::steady_clock::now();
-            elapsed_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
-            sum2 += elapsed_ns.count();
-            std::cout << elapsed_ns.count() << " ns \n";
-        }
-    }
-
-
-
-
-    std::cout << sum1 << " ns\n";
-    std::cout << sum2 << " ns\n";
-
-    return 0;
-}
